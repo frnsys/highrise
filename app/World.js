cc3a8ffce1e789b215d5553e834ebbc8e97c9028
Francis Tseng <f+accounts@frnsys.com>
2016-12-08 16:55:04 -0500

    layout testing

--- app/World.js
+++ app/World.js
@@ -56,7 +56,7 @@ class World {
     return floor;
   }
 
-  addStairs(fromFloor, toFloor, pos, depth, rotation=0, width=4) {
+  addStairs(fromFloor, toFloor, pos, depth, width, rotation=0) {
     // creates a stair connecting fromFloor to toFloor,
     // placing it at the CCS pos, snapped-to-grid relative to the fromFloor
     // note that the pos.z is ignored; stairs are auto placed on the ground
--- main.js
+++ main.js
@@ -13,60 +13,88 @@ const cellSize = 2;
 const scene = new Scene('#stage');
 const world = new World(cellSize, scene);
 
-var layout = [
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-  [1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-  [1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-  [1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-  [1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,0,0,1,1,1,1,1,1,1,1,1,1],
-  [1,1,0,0,1,1,1,1,1,1,1,1,1,1]
+var floorLayouts = [
+  [[2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+   [2,2,2,2,2,2,2,2,1,1,1,2,1,2],
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
+  [[2,2,2,2,2,2,2,2,0,0,0,0,0,0],
+   [2,1,1,1,1,1,2,2,2,2,2,2,2,2],
+   [2,1,1,1,1,1,2,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,2,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,2,1,1,1,1,1,1,2],
+   [2,1,1,1,1,1,2,1,1,1,2,2,2,2],
+   [2,1,1,1,1,1,1,1,1,1,2,1,1,2],
+   [2,1,1,1,1,1,1,1,1,1,2,2,2,2],
+   [2,1,1,1,1,1,2,1,1,1,1,2,1,2],
+   [2,2,2,2,1,2,2,1,1,1,1,2,2,2],
+   [2,1,1,1,1,1,2,1,1,1,1,1,1,2],
+   [2,1,1,2,1,1,2,1,1,1,1,1,1,2],
+   [2,2,2,2,2,2,2,0,2,2,2,2,2,2],
+   [0,0,0,0,0,0,1,1,0,0,0,0,0,0]],
+  [[0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+   [2,2,2,2,2,2,2,0,0,0,0,0,0,0],
+   [2,1,1,2,1,1,2,0,0,0,0,0,0,0],
+   [2,1,1,1,1,1,2,0,0,0,0,0,0,0],
+   [2,2,2,2,1,1,2,0,0,0,0,0,0,0],
+   [2,1,1,2,2,1,2,0,0,0,0,0,0,0],
+   [2,1,1,1,1,1,2,0,0,0,0,0,0,0],
+   [2,1,1,2,1,1,2,0,0,0,0,0,0,0],
+   [2,2,2,2,2,2,2,0,0,0,0,0,0,0],
+   [0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 ];
 
 // birth the world
-var layoutGrid = Layout.rect(20,20);
-var f1 = world.addFloor(layout, new THREE.Vector3(-10,0,-10));
-// var f1 = world.addFloor(layoutGrid, new THREE.Vector3(-10,0,-10));
-var f2 = world.addFloor(layoutGrid, new THREE.Vector3(-10,5,-10));
-// var f3 = world.addFloor(layoutGrid, new THREE.Vector3(-10,10,-10));
-// var f4 = world.addFloor(layoutGrid, new THREE.Vector3(-10,15,-10));
-world.addStairs(f1, f2, new THREE.Vector2(5,5), 8);
-// world.addStairs(f2, f3, new THREE.Vector2(12,8), 8, Math.PI/2);
-// world.addStairs(f3, f4, new THREE.Vector2(8,8), 8);
+var floorHeight = 3;
+var floors = _.map(floorLayouts, (layout, i) => {
+  return world.addFloor(layout, new THREE.Vector3(0,i*floorHeight,0));
+});
+world.addStairs(floors[0], floors[1], new THREE.Vector2(1,3), 6, 2);
 
 // change the world
 const ui = new UI(world);
 const designer = new ObjektDesigner(cellSize, ui);
 
+var agents = [
+  new HungryGhost(world, {x:8, y:1}, floors[0], 0x6666ff)
+];
 // populate the world
 // var floors = [f1, f2, f3];
-var floors = [f1];
-var colors = [0x4286f4, 0xf4a442];
-var agents = _.map(floors, (f, i) => {
-  // var agent = new Agent(world, {x:0,y:0}, f, colors[i]);
-  // var onArrive = () => {
-  //   var target = {
-  //     x:_.random(0,rows), y:_.random(0,cols),
-  //     floor: _.sample(floors)
-  //   };
-  //   var route = agent.goTo(target, onArrive);
-  //   if (route.length === 0) {
-  //     onArrive();
-  //   }
-  //   _.each(route, leg => {
-  //     leg.surface.highlightPath(leg.path, agent.color);
-  //   });
-  // }
-  // onArrive();
-  var agent = new HungryGhost(world, {x:0,y:0}, f, colors[i]);
-  return agent;
-});
+// var floors = [f1];
+// var colors = [0x4286f4, 0xf4a442];
+// var agents = _.map(floors, (f, i) => {
+//   // var agent = new Agent(world, {x:0,y:0}, f, colors[i]);
+//   // var onArrive = () => {
+//   //   var target = {
+//   //     x:_.random(0,rows), y:_.random(0,cols),
+//   //     floor: _.sample(floors)
+//   //   };
+//   //   var route = agent.goTo(target, onArrive);
+//   //   if (route.length === 0) {
+//   //     onArrive();
+//   //   }
+//   //   _.each(route, leg => {
+//   //     leg.surface.highlightPath(leg.path, agent.color);
+//   //   });
+//   // }
+//   // onArrive();
+//   var agent = new HungryGhost(world, {x:0,y:0}, f, colors[i]);
+//   return agent;
+// });
 
 // boot the world
 var clock = new THREE.Clock();
