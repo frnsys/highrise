3ff48df9ad00046870980b746d4035230858f488
dantaeyoung <provolot@gmail.com>
2017-02-26 15:59:01 -0500

    added moment, ui working

--- subapps/ui/main.js
+++ subapps/ui/main.js
@@ -3,6 +3,7 @@ import './ui.sass';
 import $ from 'jquery';
 import _ from 'underscore';
 import io from 'socket.io-client';
+import moment from 'moment';
 
 console.log("I'm a lil ui! whee");
 
@@ -22,32 +23,71 @@ window.test2 = function() {
 
 window.socket = socket;
 
+var check_and_disable_cancelconfirm = () => {
+	if($(".selected").length) { 
+		$("button#cancel").removeAttr("disabled"); 
+	}  else {
+		$("button#cancel").attr("disabled", "disabled"); 
+	}
+	
+	if($("#actions .selected").length) {
+ 		$("button#confirm").removeAttr("disabled"); 
+	}  else {
+		$("button#confirm").attr("disabled", "disabled"); 
+	}
+   
+}
+
+var updateStatusScroll = () => {
+    $("#statuses").scrollTop($("#statuses")[0].scrollHeight);
+}
 
 $(function() {
+  check_and_disable_cancelconfirm();
 
   $("#users .button").click((event) => {
     $(event.currentTarget).toggleClass("selected");
+    check_and_disable_cancelconfirm();
   });
 
   $("#actions .button").click((event) => {
-    console.log($(event.currentTarget).attr("id"));
-    $("#users .button.selected").each((i, e) => {
-      console.log($(e).attr("id"));
-    });
+    $("#actions .button").not(event.currentTarget).removeClass("selected");
+    $(event.currentTarget).toggleClass("selected");
+    check_and_disable_cancelconfirm();
+	});
 
-    var s=""
-    s += $(event.currentTarget).attr("id");
-    $("#users .button.selected").each((i, e) => {
-      s += $(e).attr("id");
-    });
-
-    socket.emit('broadcast', {'data': s});
-
-    // TODO: make compatible and do bidirectional communication
-
-    console.log(s)
-
-
+  $("button#cancel").click((event) => {
+    $(".selected").removeClass("selected");
+    check_and_disable_cancelconfirm();
   });
+
+  $("button#confirm").click((event) => {
+
+    var data = {};
+    data.sender = "ui";
+    data.action = $("#actions .button.selected").attr("id");
+    data.users = [];
+    data.dataentry_time = moment().format();
+
+
+		$("#users .button.selected").each((i, e) => {
+			data.users.push($(e).attr("id"));
+		});
+
+		socket.emit('broadcast', data);
+
+    $("#statuses").append("<li>" 
+      + moment(data.dataentry_time).format("YYYY MMM Do h:mm:ss a") + " --- " 
+      + data.action + " :: " 
+      + data.users.join(", ") 
+    + "</li>")
+    updateStatusScroll();
+
+    $(".selected").removeClass("selected");
+    check_and_disable_cancelconfirm();
+  });
+
+
+
 });
 
