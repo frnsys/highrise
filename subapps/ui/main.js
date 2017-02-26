254c90ef5d21a94c8fb52c9f9f09a4ad23a572f3
dantaeyoung <provolot@gmail.com>
2017-02-26 16:11:11 -0500

    a few comments

--- subapps/ui/main.js
+++ subapps/ui/main.js
@@ -5,23 +5,13 @@ import _ from 'underscore';
 import io from 'socket.io-client';
 import moment from 'moment';
 
-console.log("I'm a lil ui! whee");
 
 var socket = io();
-
+window.socket = socket; // fer debugging
 socket.on('message', function(data) {
   console.log(data);
 });
 
-window.test = function() {
-  socket.emit('echo', {'data': 'foo!'});
-}
-
-window.test2 = function() {
-  socket.emit('broadcast', {'data': 'foo!'});
-}
-
-window.socket = socket;
 
 var check_and_disable_cancelconfirm = () => {
 	if($(".selected").length) { 
@@ -35,47 +25,52 @@ var check_and_disable_cancelconfirm = () => {
 	}  else {
 		$("button#confirm").attr("disabled", "disabled"); 
 	}
-   
 }
 
 var updateStatusScroll = () => {
     $("#statuses").scrollTop($("#statuses")[0].scrollHeight);
 }
 
+
+
 $(function() {
   check_and_disable_cancelconfirm();
 
+  // select users
   $("#users .button").click((event) => {
     $(event.currentTarget).toggleClass("selected");
     check_and_disable_cancelconfirm();
   });
 
+  // select actions
   $("#actions .button").click((event) => {
     $("#actions .button").not(event.currentTarget).removeClass("selected");
     $(event.currentTarget).toggleClass("selected");
     check_and_disable_cancelconfirm();
 	});
 
+  // 'cancel' button
   $("button#cancel").click((event) => {
     $(".selected").removeClass("selected");
     check_and_disable_cancelconfirm();
   });
 
+  // confirm button
   $("button#confirm").click((event) => {
 
     var data = {};
     data.sender = "ui";
     data.action = $("#actions .button.selected").attr("id");
-    data.users = [];
     data.dataentry_time = moment().format();
-
-
+    data.users = [];
 		$("#users .button.selected").each((i, e) => {
 			data.users.push($(e).attr("id"));
 		});
 
+    // send data
 		socket.emit('broadcast', data);
 
+    // show status
     $("#statuses").append("<li>" 
       + moment(data.dataentry_time).format("YYYY MMM Do h:mm:ss a") + " --- " 
       + data.action + " :: " 
@@ -86,7 +81,6 @@ $(function() {
     $(".selected").removeClass("selected");
     check_and_disable_cancelconfirm();
   });
-
 
 
 });
