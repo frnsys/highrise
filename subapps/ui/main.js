a91e8a5f081aa1c27f074bc807fc06342912e359
dantaeyoung <provolot@gmail.com>
2017-02-25 19:57:32 -0500

    ui changes and initial js handling

--- subapps/ui/main.js
+++ subapps/ui/main.js
@@ -1,10 +1,10 @@
+import '~/css/reset.sass';
 import './ui.sass';
 import $ from 'jquery';
 import _ from 'underscore';
 import io from 'socket.io-client';
 
-console.log("I'm a lil ui!");
-
+console.log("I'm a lil ui! whee");
 
 var socket = io();
 
@@ -20,6 +20,34 @@ window.test2 = function() {
   socket.emit('broadcast', {'data': 'foo!'});
 }
 
-
 window.socket = socket;
+
+
+$(function() {
+
+  $("#users .button").click((event) => {
+    $(event.currentTarget).toggleClass("selected");
+  });
+
+  $("#actions .button").click((event) => {
+    console.log($(event.currentTarget).attr("id"));
+    $("#users .button.selected").each((i, e) => {
+      console.log($(e).attr("id"));
+    });
+
+    var s=""
+    s += $(event.currentTarget).attr("id");
+    $("#users .button.selected").each((i, e) => {
+      s += $(e).attr("id");
+    });
+
+    socket.emit('broadcast', {'data': s});
+
+    // TODO: make compatible and do bidirectional communication
+
+    console.log(s)
+
+
+  });
+});
 
