1ed9243f053bf0de559b7a73f5c1827038f4b840
Francis Tseng <f+accounts@frnsys.com>
2017-03-26 12:56:59 -0400

    halving frame rate to 30FPS for smoother performance

--- subapps/sim/main.js
+++ subapps/sim/main.js
@@ -165,15 +165,13 @@ var agents = [
   // }, world)
 ];
 
-/*test to try out a lot of people************
-/**
 function randomString(length, chars) {
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
     return result;
 }
 
-for(var i = 0; i < 20; i++) {
+for(var i = 0; i < 10; i++) {
 	agents.push(new PartyGoer(randomString(10, 'abcdefghijklmnopqrstuvwxyz'), {
     bladder: _.random(100),
     hunger: _.random(100),
@@ -186,7 +184,6 @@ for(var i = 0; i < 20; i++) {
     topicPreference: [_.random(-1, 1), _.random(-1,1)]
   }, world))
 }
-**************/
 
 var colors = [0xff0000, 0x0000ff];
 
@@ -218,11 +215,13 @@ log.setLevel('error');
 
 // boot the world
 var clock = new THREE.Clock();
+var elapsedFrames = 0;
 function run() {
   requestAnimationFrame(run);
-  scene.render();
   var delta = clock.getDelta();
-  if (delta < 0.5) {
+  // every other frame so we only render at 30fps
+  if (delta < 0.5 && elapsedFrames % 2 == 0) {
+      scene.render();
     // if the delta is really large,
     // (i.e. when the tab loses focus)
     // agents will take very large steps
@@ -237,6 +236,7 @@ function run() {
     ui.update();
 		if(typeof(thisSimulationScreen) !== "undefined") { thisSimulationScreen.update(); }
   }
+  elapsedFrames++;
 }
 run();
 log.setLevel('error');
